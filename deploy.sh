d9dfd6366ced03694444c7adb090ce02c674e10c
rutvikpensionwar <pensionwar.rutvik@gmail.com>
2019-02-18 19:07:48 -0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t rutvikpensionwar/multi-client:latest -t rutvikpensionwar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rutvikpensionwar/multi-server:latest -t rutvikpensionwar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rutvikpensionwar/multi-worker:latest -t rutvikpensionwar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push rutvikpensionwar/multi-client:latest
+docker push rutvikpensionwar/multi-server
+docker push rutvikpensionwar/multi-worker
+
+docker push rutvikpensionwar/multi-client:$SHA
+docker push rutvikpensionwar/multi-server:$SHA
+docker push rutvikpensionwar/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=rutvikpensionwar/multi-client:$SHA
+kubectl set image deployments/server-deployment server=rutvikpensionwar/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=rutvikpensionwar/multi-worker:$SHA
